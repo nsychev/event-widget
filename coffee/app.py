da1e682568af93f70880780663cbb3780b53320a
Nikita Sychev <root@nsychev.ru>
2021-07-12 17:36:49 +0300

    believe https

--- coffee/app.py
+++ coffee/app.py
@@ -2,6 +2,8 @@ import os
 
 import flask
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 from . import blueprints
 from .config import load_config
 from .storage.utils import install_database
@@ -9,6 +11,7 @@ from .storage.utils import install_database
 
 def create_app():
     app = flask.Flask(__name__)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1)
     app.config.from_mapping(load_config(os.path.dirname(app.root_path)))
     install_database(app)
 
