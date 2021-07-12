dc02398c8ce2a5157030005f9be2dc11ee6d0ac1
Nikita Sychev <root@nsychev.ru>
2021-07-12 20:56:25 +0300

    fix distinct

--- coffee/blueprints/coffee/routes.py
+++ coffee/blueprints/coffee/routes.py
@@ -18,7 +18,7 @@ def main():
              .filter(Event.available == True)
              .join(Section, Event.sections, isouter=True)
              .join(Reservation, Section.reservations, isouter=True)
-             .distinct(Section.name, Reservation.name)
+             .distinct(Section.id, Reservation.name)
              .filter(Reservation.user_id == None)
              .options(
                  contains_eager(Event.sections),
